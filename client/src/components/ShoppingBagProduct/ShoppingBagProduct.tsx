396ad23332377fc4a50733009edc84a812f9192a
Gourav <gourav.g@avon.com>
2023-05-12 12:54:07 +0530

    small ui fixes in cart

--- client/src/components/ShoppingBagProduct/ShoppingBagProduct.tsx
+++ client/src/components/ShoppingBagProduct/ShoppingBagProduct.tsx
@@ -4,9 +4,12 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import { Remove, Add } from '@mui/icons-material';
 import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
 
 export default function ShoppingBagProduct({lineItem, decrement, increment, updateQuantity, removeItem}: any) {
   const [quantity, setQuantity] = useState(lineItem.quantity);
+  const width = window.innerWidth;
 
   const changeQuantity = (lineItemId: Number, inputQuantity: string) => {
     let itemQuantity = parseInt(inputQuantity);
@@ -44,20 +47,40 @@ export default function ShoppingBagProduct({lineItem, decrement, increment, upda
   return (
     <div className={styles.products__product}>
       <div className={styles.products__product__image}>
-        <img src={lineItem.thumbnail} width="125px" height="125px"/>
+        <img src={lineItem.thumbnail} />
       </div>
       <div className={styles.products__product__name}>
         <h3>{lineItem.name}</h3>
         {lineItem.size && <div>Size: {lineItem.size}</div>}
         <p>{lineItem.description}</p>
         <div className={styles.products__product__name__counter}>
-          <IconButton onClick={(e) => decrementQuantity(lineItem.productId, quantity)}>
-            <Remove/>
-          </IconButton>
-          <TextField size="small" value={quantity} inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} onChange={(e) => changeQuantity(lineItem.productId, e.target.value)}/>
-          <IconButton onClick={(e) => incrementQuantity(lineItem.productId, quantity)}>
-            <Add/>
-          </IconButton>
+          {
+            width < 668 ?
+              <>
+                <Select
+                  size="small"
+                  value={quantity}
+                  displayEmpty
+                  onChange={(e) => changeQuantity(lineItem.productId, e.target.value)}
+                  sx={{flexGrow: 1}}
+                >
+                  {
+                      [...Array(10).keys()].map((value) => (
+                        <MenuItem value={value + 1}>{value + 1}</MenuItem>
+                      ))
+                  }
+                </Select>
+              </> :
+              <>
+                <IconButton onClick={(e) => decrementQuantity(lineItem.productId, quantity)}>
+                  <Remove/>
+                </IconButton>
+                <TextField size="small" value={quantity} inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} onChange={(e) => changeQuantity(lineItem.productId, e.target.value)}/>
+                <IconButton onClick={(e) => incrementQuantity(lineItem.productId, quantity)}>
+                  <Add/>
+                </IconButton>
+              </>
+          }
           <Button variant="contained" size="small" onClick={(e) => removeItem(lineItem.productId)} sx={{marginLeft: '1rem'}}>Remove</Button>
         </div>
       </div>
