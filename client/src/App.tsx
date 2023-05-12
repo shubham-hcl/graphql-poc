af2dfa1ce742dac4388a2c33cb1f5ec9e0f735cc
Shubham Kaushik <shubham.kaushik@avon.com>
2023-05-12 23:48:12 +0530

    host url

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,7 @@ import AppProvider from './providers/AppProvider'
 import ProductDetail from './components/ProductDetail'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: 'https://main.d2g2z5kdz5rpse.amplifyapp.com/graphql', // Change to http://localhost:4000/graphql for dev env
   cache: new InMemoryCache(),
 })
 
